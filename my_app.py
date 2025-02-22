db52b4a28dd3884a4ad9501dd79323febf62f944
Fabio Marquez <fabio.marquezt@gmail.com>
2025-02-22 17:09:28 -0500

    Primer commit: pasos iniciales de Flask

---
+++ my_app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return '<h1>RUTA PRINCIPAL</h1>'
+
+
+@app.route('/my_app')
+@app.route('/my_app/<name>')
+@app.route('/my_app/<name>/<int:age>')
+def my_app(name = None, age = None):
+    
+    if name == None and age == None:
+        return '<h1>No hay edad<h1>'
+    elif age == None:
+        return f'<h1>Hola,{name}</h1>'
+    else:
+        return f'<h1>Hola, tu nombre es {name} y tu edad es {age}!</h1>'
+    
+from markupsafe import escape    
+@app.route('/code/<path:code>')
+def code(code):
+    return f'<code>{escape(code)}</code>'
